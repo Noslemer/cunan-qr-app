0ac62a277d72d0f4183ec9e788a8ffebff59822f
Nighty1809 <chaseracines24@gmail.com>
2025-06-20 17:59:48 +0800

    update

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,17 +1,15 @@
 <template>
     <div class="d-flex align-center justity-center" style="height: 100vh;">
       <v-card width="500" elevation="10" class="pa-4">
-        <h2 class="text-center bue--text text--darken-3 mb-4">Sign in</h2>
-        <v-card-text>
+            <v-card-text>
             <v-form>
                 <v-text-field
                  outlined
-                 label=" username"
-                 type=" username"
+                 label=" Email"
                 ></v-text-field>
                 <v-text-field
+                 label=" Password"
                  outlined
-                 label=" password"
                  type=" password"
                  prepend-inner-icon="mdi-lock"
                 ></v-text-field>
